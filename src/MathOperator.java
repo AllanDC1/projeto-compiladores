3a961ecb3c088e5461b2e99b60e978e728ca64f1
Allan Donetti Calen <allan.calen@paysmart.com.br>
2026-04-30 14:55:34 -0300

    Ajustes gerais

---
+++ src/MathOperator.java
@@ -0,0 +1,43 @@
+package src;
+
+import java.text.CharacterIterator;
+
+public class MathOperator extends AFD {
+
+    @Override
+    public Token evaluate(CharacterIterator code) {
+
+        switch (code.current()) {
+            case '+' -> {
+                code.next();
+                return new Token("PLUS", "+");
+            }
+            case '-' -> {
+                code.next();
+                return new Token("MINUS", "-");
+            }
+            case '*' -> {
+                code.next();
+                return new Token("MUL", "*");
+            }
+            case '/' -> {
+                code.next();
+                return new Token("DIV", "/");
+            }
+            case '(' -> {
+                code.next();
+                return new Token("LPAREN", "(");
+            }
+            case ')' -> {
+                code.next();
+                return new Token("RPAREN", ")");
+            }
+            case CharacterIterator.DONE -> {
+                return new Token("EOF", "$");
+            }
+            default -> {
+                return null;
+            }
+        }
+    }
+}
---
+++ src/Token.java
@@ -0,0 +1,16 @@
+package src;
+
+public class Token {
+    public String tipo;
+    public String lexema;
+
+    public Token(String tipo, String lexema) {
+        this.tipo = tipo;
+        this.lexema = lexema;
+    }
+
+    @Override
+    public String toString() {
+        return "<" + tipo + ", " + lexema + ">";
+    }
+}
